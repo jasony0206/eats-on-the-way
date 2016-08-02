98937594c69de01ad34b42a635f1dd468d844b24
Jason Yoon <jasony0206@gmail.com>
2016-08-02 16:18:07 -0700

    Start implementing PathFinder.find_path

--- app/services/path_finder.rb
+++ app/services/path_finder.rb
@@ -1,4 +1,15 @@
-class PathFinder
+require 'open-uri'
+
+module PathFinder
+  @@GOOGLE_API_KEY = ENV['GOOGLE_API_KEY']
+  @@MAPS_URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s'
+
   def self.find_path(origin, destination)
+    encoded_origin = CGI::escape(origin)
+    encoded_destination = CGI::escape(destination)
+    maps_url = @@MAPS_URL_TEMPLATE % [encoded_origin, encoded_destination, @@GOOGLE_API_KEY]
+
+    result = JSON.parse(open(maps_url).read)
+    steps = result['routes'].first['legs'].first['steps']
   end
 end
--- spec/services/path_finder_spec.rb
+++ spec/services/path_finder_spec.rb
@@ -4,7 +4,6 @@ describe PathFinder do
   describe '.find_path' do
     let(:origin) { 'Disneyland' }
     let(:destination) { 'Universal Studios Hollywood' }
-    let(:google_api_key) { ENV['GOOGLE_API_KEY'] }
 
     it 'should find a path from origin to destination' do
       path = described_class.find_path(origin, destination)
