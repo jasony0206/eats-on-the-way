40e721cd057f25d2f3e8024ef71aabd08d815142
Jason Yoon <jasony0206@gmail.com>
2016-08-09 07:21:10 -0700

    Start implementing RestaurantSearchService

---
+++ app/services/restaurant_search_service.rb
@@ -0,0 +1,22 @@
+module RestaurantSearchService
+  def self.find_restaurants(origin, destination)
+    directionsApiResponse = GoogleMapsClient.find_directions(origin, destination)
+    google_coordinates = MapsApiProcessor.extract_coordinates(directionsApiResponse)
+    yelp_coordinates = convert_hash_keys(google_coordinates)
+
+    restaurants = yelp_coordinates.map do |point|
+      yelpApiResponse = YelpClient.search(point, { term: 'food' })
+      YelpApiProcessor.extract_businesses(yelpApiResponse)
+    end
+    restaurants.flatten!
+  end
+
+  def self.convert_hash_keys(google_coordinates)
+    yelp_coordinates = google_coordinates.map do |google_point|
+      yelp_point = {}
+      yelp_point[:latitude] = google_point['lat']
+      yelp_point[:longitude] = google_point['lng']
+      yelp_point
+    end
+  end
+end
