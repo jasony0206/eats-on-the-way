8a51332fd636b8bdc631f82b6361f82bc8e4897b
Jason Yoon <jasony0206@gmail.com>
2016-08-14 00:26:11 -0700

    Implement MapsClient.distance_matrix

--- app/services/google_maps_client.rb
+++ app/services/google_maps_client.rb
@@ -2,13 +2,29 @@ require 'open-uri'
 
 module GoogleMapsClient
   @@GOOGLE_API_KEY = ENV['GOOGLE_API_KEY']
-  @@MAPS_URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s'
+  @@DIRECTIONS_URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s'
+  @@DISTANCE_MATRIX_URL_TEMPLATE = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=%s&destinations=%s&key=%s'
 
   def self.find_directions(origin, destination)
     encoded_origin = CGI::escape(origin)
     encoded_destination = CGI::escape(destination)
-    maps_url = @@MAPS_URL_TEMPLATE % [encoded_origin, encoded_destination, @@GOOGLE_API_KEY]
+    maps_url = @@DIRECTIONS_URL_TEMPLATE % [encoded_origin, encoded_destination, @@GOOGLE_API_KEY]
 
     result = JSON.parse(open(maps_url).read)
+  end
+
+  def self.distance_matrix(origins, destinations)
+    formatted_origins = origins.map do |coord|
+      coord.values.join(",")
+    end
+    origins_string = formatted_origins.join("|")
+
+    formatted_destinations = destinations.map do |coord|
+      coord.values.join(",")
+    end
+    destinations_string = formatted_destinations.join("|")
+
+    distance_matrix_url = @@DISTANCE_MATRIX_URL_TEMPLATE % [origins_string, destinations_string, @@GOOGLE_API_KEY]
+    result = JSON.parse(open(distance_matrix_url).read)
   end
 end
