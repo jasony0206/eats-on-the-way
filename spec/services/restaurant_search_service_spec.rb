0529ebd13166e6f19891f407b98b6e6291e5c080
Jason Yoon <jasony0206@gmail.com>
2016-08-17 16:06:11 -0700

    Validate RestaurantSearchService.search JSON response with schema

--- spec/services/restaurant_search_service_spec.rb
+++ spec/services/restaurant_search_service_spec.rb
@@ -15,4 +15,14 @@ describe RestaurantSearchService do
       expect(@restaurants.count).to eq(@restaurants.uniq.count)
     end
   end
+
+  describe '.search' do
+    let(:mock_response) { IO.read(Rails.root.join("spec", "fixtures", "mock_search_api_response.json")) }
+
+    it 'returned JSON response should be of valid schema' do
+      json_schema_path = 'spec/fixtures/search_response_schema.json'
+      is_valid = JSON::Validator.validate(json_schema_path, mock_response)
+      expect(is_valid).to be true
+    end
+  end
 end
